ea012acbcdb545cfc418494980bba270d285110c
paugarcia32 <paugarcia32@gmail.com>
2026-02-02 20:01:38 +0100

    chore: Add package-lock.json and update site information
    - Introduced package-lock.json for dependency management.
    - Updated site constants in src/consts.ts and related files to reflect new author details.
    - Modified footer to dynamically display the current year.

--- src/consts.ts
+++ src/consts.ts
@@ -1,44 +1,46 @@
-import type { Site, Metadata, Socials } from "@types";
+import type * as Types from "@types";
 
-export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+export const SITE: Types.Site = {
+  NAME: "paugarcia32",
+  EMAIL: "paugarcia32@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
-export const HOME: Metadata = {
+export const HOME: Types.Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION:
+    "paugarcia32.dev is a minimal and lightweight blog and portfolio.",
 };
 
-export const BLOG: Metadata = {
+export const BLOG: Types.Metadata = {
   TITLE: "Blog",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
-export const WORK: Metadata = {
+export const WORK: Types.Metadata = {
   TITLE: "Work",
   DESCRIPTION: "Where I have worked and what I have done.",
 };
 
-export const PROJECTS: Metadata = {
+export const PROJECTS: Types.Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION:
+    "A collection of my projects, with links to repositories and demos.",
 };
 
-export const SOCIALS: Socials = [
-  { 
+export const SOCIALS: Types.Socials = [
+  {
     NAME: "twitter-x",
     HREF: "https://twitter.com/markhorn_dev",
   },
-  { 
+  {
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/paugarcia32",
   },
-  { 
+  {
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
+    HREF: "https://www.linkedin.com/in/paugarcia32",
+  },
 ];
